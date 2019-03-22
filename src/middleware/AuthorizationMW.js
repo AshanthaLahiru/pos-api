cf99283d3f85c75d45c70225ea98e7c7758f3f3b
ashanthalahiru <ashantha.lahiru@gmail.com>
2019-03-22 16:58:54 +0530

    Updated authorization middleware

--- src/middleware/AuthorizationMW.js
+++ src/middleware/AuthorizationMW.js
@@ -10,20 +10,19 @@ export default class AuthorizationMW {
 
     verifyToken(req, res, next) {
         let header = req.header("Authorization");
-        // if (header) {
-        //     let token = header.split(' ')[1];
-        //     try {
-        //         if (self.jwt.verify(token, self.constants.jwtSecretKey)) {
-        //             next();
-        //         }
-        //     }
-        //     catch (e) {
-        //         console.log(e);
-        //         next(res.status(401).json("Authorization Failed"));
-        //     }
-        // } else {
-        //     next(res.status(401).json("Authorization Failed"));
-        // }
-        next();
+        if (header) {
+            let token = header.split(' ')[1];
+            try {
+                if (self.jwt.verify(token, self.constants.jwtSecretKey)) {
+                    next();
+                }
+            }
+            catch (e) {
+                console.log(e);
+                next(res.status(401).json("Authorization Failed"));
+            }
+        } else {
+            next(res.status(401).json("Authorization Failed"));
+        }
     }
 }
