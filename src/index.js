5ea22e64e17abf4fa5c218a155855893983861a7
ashanthalahiru <ashantha.lahiru@gmail.com>
2019-04-02 08:43:33 +0530

    Added heroku requirements

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.use(function (err, req, res, next) {
   });
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
 });
